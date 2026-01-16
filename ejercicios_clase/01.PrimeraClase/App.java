0346d3b07781de176fb727ff8b19729179705a74
AgustinICAI <a.g.lamela@gmail.com>
2026-01-16 09:29:14 +0100

    subiendo codigo ejemplo constructores

---
+++ ejercicios_clase/01.PrimeraClase/App.java
@@ -0,0 +1,11 @@
+class App {
+  public static void main(String[] args) {
+    System.out.println("Creando nuestra primera clase");
+
+    Animal p1 = new Animal("pollo",40,30);
+    System.out.println(p1.getInfo());
+    p1.mover();
+    System.out.println(p1.getInfo());
+
+  }
+}
---
+++ ejercicios_clase/02.Constructores/Producto.java
@@ -0,0 +1,9 @@
+public class Producto {
+  String tipo;
+  double precio;
+
+  Producto(String _tipo){
+    tipo = _tipo;
+  }
+
+}
